6ac3dfba44d13c4c96ffaa3812a65a5464f5c2fa
marcusG4m4 <102686934+marcusG4m4@users.noreply.github.com>
2023-11-03 13:16:55 -0300

    apredizado de 1 a 8

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,52 @@
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.4);
+console.log(typeof -127);
+
+// 2 - Ops. Aritiméticas
+console.log(2 + 4);
+console.log(38 - 6);
+console.log(15 * 89);
+console.log(1583 / 8);
+
+console.log(5 + 4 * 2);
+
+// 3 - Special Numbers
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "asd");
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log(`Mais um texto`);
+console.log(`13`);
+console.log(typeof "Um texto");
+console.log(typeof `Mais um texto`);
+
+// 5 - Simbolos especiais em strings
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento \tde  tab");
+
+//6 - Concatenação
+console.log("oi, " + "tudo " + "bem?");
+console.log("testando " + "com " + "crase?");
+
+// 7 - Template strings
+console.log(`a soma de 2+2 é: ${2 + 2}`);
+console.log(`a soma de 2+6 é: ${2 + 6}`);
+console.log(`a soma de 5*5 é: ${5 * 5}`);
+console.log(`a soma de 2*9 é: ${2 * 9}`);
+
+// 8 - Boolean
+console.log(true);
+
+console.log(5>20);
+
+console.log(30>10);
+
+console.log(typeof true);
+
