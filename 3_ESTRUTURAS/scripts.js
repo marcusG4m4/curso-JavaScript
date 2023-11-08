42ab04e088bb4ed8adef42611f2d326c22803e65
marcusG4m4 <102686934+marcusG4m4@users.noreply.github.com>
2023-11-08 12:27:19 -0300

    variáveis

---
+++ 3_ESTRUTURAS/scripts.js
@@ -0,0 +1,44 @@
+// 1 - Variáveis
+let nome = "Marcus";
+
+console.log(nome);
+
+nome = "Marcus José";
+
+console.log(nome);
+
+const idade = 42;
+
+console.log(idade);
+
+//idade = 41;
+
+console.log(typeof nome)
+
+console.log(typeof idade)
+
+// 2 -  mais sobre variáveis
+//let 2teste = "inválido";
+//let @teste = "inválido";
+
+let a=10,
+b=20,
+c=30;
+
+console.log( a,b,c);
+
+const nomecompleto = "Marcus José";
+
+const nomeCompleto = "Lucas Gama";
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "ok";
+
+console.log(_teste);
+
+console.log($teste);
