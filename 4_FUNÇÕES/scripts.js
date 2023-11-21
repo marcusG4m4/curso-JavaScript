83d42f889a5d7c348cedd6ad7da6754348b24099
marcusG4m4 <102686934+marcusG4m4@users.noreply.github.com>
2023-11-21 01:57:34 -0300

    return

--- 4_FUNÇÕES/scripts.js
+++ 4_FUNÇÕES/scripts.js
@@ -24,3 +24,34 @@ function saudacao() {
 }
 
 saudacao();
+
+// 2 - Return
+
+const a = 15;
+const b = 26;
+const c = 37;
+const d = 42;
+
+function soma( n1, n2){
+    return n1 + n2;
+}
+
+const resultado = soma(b, d); 
+
+console.log(resultado);
+
+console.log(soma(b, c));
+
+
+// exemplo:
+const e = 5;
+const f = 6;
+const g = 3;
+const h = 7;
+
+function multiplicacao(m1, m2) {
+    return m1 * m2;
+}
+const resultado = multiplicacao(e, g);
+
+console.log(resultado);
