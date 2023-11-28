a256013a1e9cd9ec6a314f030c3df43bce9640b7
marcusG4m4 <102686934+marcusG4m4@users.noreply.github.com>
2023-11-28 11:36:28 -0300

    arrays e métodos

---
+++ 5_ARREYS-E-OBJETOS/scripts.js
@@ -0,0 +1,54 @@
+// 1 - Arrays
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(lista);
+
+console.log(typeof lista);
+
+const itens = ["marcus", true, 5, 4.12, []];
+
+console.log(itens);
+
+// 2 - mais sobre arrys 
+const arr = ["a","b","c", "d", "e",];
+
+console.log(arr[0]);
+
+console.log(arr[3]);
+
+console.log(arr[9]);
+
+// 3 - propriedades
+const numbers = [5, 6 , 9 , 8];
+
+console.log(numbers.length);
+
+console.log(numbers["length"]);
+
+const myName = "marcus";
+
+console.log(myName.length);
+
+// 4 - métodos
+const otherNumbers = [7, 1, 2, 3, 4,];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+//exemplo
+const names = ["marcus","markelys",];
+
+const otherNames = ["jean","wallace","nikson",];
+
+const allNames = names.concat(otherNames);
+
+console.log(allNames);
+
+//UpperCase
+const text = "AlGum tExto";
+
+console.log(text.toUpperCase());
+
+console.log(text.indexOf("u"));
+
